bc8fb598f97505e549ffa3c29070f94219134956
SumitSahu444 <sumitkumarsahu141@gmail.com>
2025-08-16 23:13:40 +0530

    hero section done

--- components/sub/hero-content.tsx
+++ components/sub/hero-content.tsx
@@ -24,30 +24,32 @@ export const HeroContent = () => {
         >
           <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
           <h1 className="Welcome-text text-[13px]">
-            Fullstack Developer Portfolio
+            Fullstack Developer
           </h1>
         </motion.div>
+<motion.div
+  variants={slideInFromLeft(0.5)}
+  className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
+>
+  <span>
+    Crafting{" "}
+    <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
+      impactful
+    </span>{" "}
+    digital solutions.
+  </span>
+</motion.div>
 
-        <motion.div
-          variants={slideInFromLeft(0.5)}
-          className="flex flex-col gap-6 mt-6 text-6xl text-bold text-white max-w-[600px] w-auto h-auto"
-        >
-          <span>
-            Providing{" "}
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
-              the best
-            </span>{" "}
-            project experience.
-          </span>
-        </motion.div>
+<motion.p
+  variants={slideInFromLeft(0.8)}
+  className="text-lg text-gray-400 my-5 max-w-[600px]"
+>
+  I&apos;m a passionate Full Stack Developer specializing in building
+  scalable web and mobile applications. Skilled in modern technologies
+  and problem-solving, I focus on creating solutions that deliver real
+  value. Explore my work to see how I turn ideas into reality.
+</motion.p>
 
-        <motion.p
-          variants={slideInFromLeft(0.8)}
-          className="text-lg text-gray-400 my-5 max-w-[600px]"
-        >
-          I&apos;m a Full Stack Software Engineer with experience in Website,
-          Mobile, and Software development. Check out my projects and skills.
-        </motion.p>
 
         <motion.a
           variants={slideInFromLeft(1)}
